bbdaaa8be6f211feaa946cf270ae5e5978d656c9
maksarts <maksarts@mail.ru>
2024-08-15 17:01:01 +0400

    Docker-compose deploy, db init, application properties using .env

--- init.sql
+++ init.sql
@@ -20,15 +20,16 @@ CREATE TABLE comment (
                          id SERIAL PRIMARY KEY,
                          author_email VARCHAR(100) REFERENCES employee(email),
                          task_id INTEGER REFERENCES task(id),
-                         content VARCHAR(1000)
-);
-
-CREATE TABLE comment (
-                         id SERIAL PRIMARY KEY,
-                         author_email VARCHAR(100) REFERENCES employee(email),
-                         task_id INTEGER REFERENCES task(id),
                          content VARCHAR(1000),
                          created_ts TIMESTAMP
+);
+
+CREATE TABLE refresh_token (
+                               id SERIAL PRIMARY KEY,
+                               employee_email VARCHAR(100) REFERENCES employee(email),
+                               token VARCHAR(500),
+                               expiry_date TIMESTAMP,
+                               revoked BOOLEAN
 );
 
 CREATE TABLE token (
